963346f328c78c4f9f3a5de95e51e0324a8f5ed4
Paul Worthington <pworthington17@gmail.com>
2024-07-06 06:56:28 +0000

    lesson complete

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,32 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    // code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
 
 // Arrow Function With Parameters
+const addTwoNumbersArrow = (a, b) => {
+    return a + b
+}
+let arrowSum = addTwoNumbersArrow (3, 5)
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbersArrowSingle = (a, b) => a + b;
+//const addTwoNumbersArrowSingle = (a, b) => (a + b); // also valid
 
 // Implicit Returns
+const saySomething = message => console.log(message) // single parameter no need for ()
+
+const sayHello = () => console.log('hello') // empty parameters needed
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+        This is a paragraph
+    </p>`
+
+)
+// () needed on multiline returns
